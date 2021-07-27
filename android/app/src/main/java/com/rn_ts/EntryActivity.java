5b042bfe9cabaf531706a36311830785dae32b17
zhuliminl <zhuliminl@gmail.com>
2021-07-27 20:40:51 +0800

    画一个最简单的按钮

--- android/app/src/main/java/com/rn_ts/EntryActivity.java
+++ android/app/src/main/java/com/rn_ts/EntryActivity.java
@@ -1,13 +1,19 @@
 package com.rn_ts;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
 
 public class EntryActivity extends AppCompatActivity {
+    private static final String TAG = "EntryActivity";
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: ");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry);
     }
