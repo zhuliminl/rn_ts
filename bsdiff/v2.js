6404517e2bf5ee3063be65f1e0e8a47d317d0e35
zhuliminl <zhuliminl@gmail.com>
2021-08-24 02:01:54 +0800

    update .gitignore

--- bsdiff/v2.js
+++
@@ -1,5 +0,0 @@
-console.log('saulkjjjjjjjjjjjjjjjjjjjjjjj')
-console.log('bbbbbbbbbbbbbbbbbbbbb')
-console.log('bbbbbbbbbbbbbbbbbbbbb')
-console.log('bbbbbbbbbbbbbbbbbbbbb')
-console.log('bbbbbbbbbbbbbbbbbbbbb')
