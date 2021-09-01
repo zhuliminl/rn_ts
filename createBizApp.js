591fb154d22b0fd5590a782ca49540ee5dee8207
zhuliminl <zhuliminl@gmail.com>
2021-09-01 15:46:44 +0800

    readme

--- createBizApp.js
+++ createBizApp.js
@@ -2,25 +2,33 @@
 const fs = require('fs');
 var faker = require('faker');
 
-const bizConfigStr = process.argv[2];
-// console.log('hhhhhh', bizConfigStr)
-// return
-const bizConfig = JSON.parse(bizConfigStr)
+const bizConfigStr = process.argv[2] || '{}'
+const bizConfig = JSON.parse(bizConfigStr) || {}
+
 const {
-  biz = '',
-  desc = '',
-  logoUrl = '',
+  biz = "ServiceVideo",
+  desc = '短视频',
+  logoUrl = 'http://www.baidu.com',
   imgs = [],
-  textSize = 1
+  textSize = 1000,
 } = bizConfig
 
+const randomColor = () => {
+  let letters = '0123456789ABCDEF'
+  let color = '#'
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)]
+  }
+  return color
+}
 
 
 
-const textView = [...Array(textSize * 100)].map(_ => {
+
+const textView = [...Array(textSize * 1)].map(_ => {
   var randomName = faker.name.findName()
-  return `<Text>${randomName}</Text>`
-}).join('')
+  return `<Text style={styles.fake_txt}>${randomName}</Text>`
+}).join('\n')
 
 
 
@@ -28,27 +36,52 @@ const textView = [...Array(textSize * 100)].map(_ => {
 // 页面模版
 const indexTep = `
 import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, ScrollView, Text, StyleSheet } from 'react-native'
+
 const styles = StyleSheet.create({
   wraper: {
+    backgroundColor: '#FFF',
+    padding: 20,
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+  desc: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#999',
+    marginBottom: 30,
+
+  },
+  border: {
+    width: '100%',
+    height: 1,
+    backgroundColor: '#EEE'
+  },
+  fake_txt: {
+    color: '${randomColor()}'
   }
 })
 
+
 export default () => {
     return (
-      <View
+      <ScrollView
         style={styles.wraper}
       >
-        <Text>
+        <Text style={styles.header}>
         ${biz}
         </Text>
-        <Text>
+        <View style={styles.border}></View>
+        <Text style={styles.desc}>
         ${desc}
         </Text>
         <View>
         ${textView}
         </View>
-      </View>
+      </ScrollView>
     )
 }
 `
