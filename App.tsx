dc51aeff18c3ee19b02fea37a6d0f045f4727820
zhuliminl <zhuliminl@gmail.com>
2021-09-01 15:38:09 +0800

    readmem

--- App.tsx
+++ App.tsx
@@ -1,14 +1,3 @@
-/* eslint-disable react-native/no-inline-styles */
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React, {useEffect} from 'react';
 import Orientation from 'react-native-orientation';
 import {
